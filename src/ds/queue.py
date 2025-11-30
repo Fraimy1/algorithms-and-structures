ab8b924c5ef2ea82284e6f74e3a46c6b2138a254
Fraimy1 <117653591+Fraimy1@users.noreply.github.com>
2025-12-01 02:38:11 +0300

    [add] Queue

---
+++ src/ds/queue.py
@@ -0,0 +1,51 @@
+from src.core.errors import EmptyError
+
+class Queue:
+    def __init__(self) -> None:
+        """Initialize an empty queue."""
+        self._in_stack:list = []
+        self._out_stack:list = []
+
+    def enqueue(self, x: int) -> None:
+        """Add value x to the back of the queue."""
+        self._in_stack.append(x)
+
+    def _move_in_to_out(self) -> None:
+        """
+        Moves all elements from _in_stack to _out_stack
+        """
+        if not self._out_stack:                
+            while self._in_stack:
+                self._out_stack.append(self._in_stack.pop())
+
+    def dequeue(self) -> int:
+        """
+        Remove and return the front element.
+        Raise EmptyError if the queue is empty.
+        """
+        self._move_in_to_out()
+
+        if not self._out_stack:
+            raise EmptyError("Queue is empty")
+
+        return self._out_stack.pop()
+
+    def front(self) -> int:
+        """
+        Return the front element without removing it.
+        Raise EmptyError if the queue is empty.
+        """
+        self._move_in_to_out()
+
+        if not self._out_stack:
+            raise EmptyError("Queue is empty")
+
+        return self._out_stack[-1]
+
+    def is_empty(self) -> bool:
+        """Return True if the queue has no elements."""
+        return not (self._in_stack or self._out_stack)
+
+    def __len__(self) -> int:
+        """Return number of elements in the queue."""
+        return len(self._in_stack) + len(self._out_stack)
---
+++ tests/test_queue.py
@@ -0,0 +1,48 @@
+import pytest
+from src.ds.queue import Queue
+from src.core.errors import EmptyError
+
+def test_enqueue_and_front():
+    queue = Queue()
+    assert queue.is_empty()
+    
+    queue.enqueue(1)
+    queue.enqueue(2)
+    queue.enqueue(3)
+
+    assert not queue.is_empty() 
+    assert queue.front() == 1
+
+def test_dequeue():
+    queue = Queue()
+    queue.enqueue(1)
+    queue.enqueue(2)
+    queue.enqueue(3)
+
+    assert queue.dequeue() == 1
+    assert queue.front() == 2
+    assert queue.dequeue() == 2
+    assert queue.front() == 3
+    assert queue.dequeue() == 3
+    assert queue.is_empty()
+
+
+def test_empty_queue():
+    queue = Queue()
+    with pytest.raises(EmptyError):
+        queue.dequeue()
+    with pytest.raises(EmptyError):
+        queue.front()
+
+def test_len():
+    queue = Queue()
+    assert len(queue) == 0
+    queue.enqueue(1)
+    assert len(queue) == 1
+    queue.enqueue(2)
+    assert len(queue) == 2
+    queue.dequeue()
+    assert len(queue) == 1
+    queue.dequeue()
+    assert len(queue) == 0
+
